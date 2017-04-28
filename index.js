b6bf9ea75200cf6d66216416c09e468774e1ecf7
Rob Weber <rob.weber87@gmail.com>
2017-04-28 09:58:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+	return kittens.push(name);
+};
+
+function destructivelyPrependKitten(name) {
+	return kittens.unshift(name);
+};
+
+function destructivelyRemoveLastKitten() {
+	return kittens.pop();
+};
+
+function destructivelyRemoveFirstKitten() {
+	return kittens.shift();
+};
+
+function appendKitten(name) {
+	var new_array = []
+	for (var i = 0; i < kittens.length;  i++) {
+	 	new_array.push(kittens[i]);
+	 }
+	new_array.push(name); 
+	return new_array;
+};
+
+function prependKitten(name) {
+	var new_array = [name]
+	for (var i = 0; i < kittens.length;  i++) {
+	 	new_array.push(kittens[i]);
+	 } 
+	return new_array;
+};
+
+function removeLastKitten() {
+	var new_array = kittens.slice(0, kittens.length - 1)
+	return new_array;
+};
+
+function removeFirstKitten() {
+	var new_array = kittens.slice(1, kittens.length)
+	return new_array;
+};
